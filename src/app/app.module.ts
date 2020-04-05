96a6f46d556ca4ea4236b4973b9a6b80b31e1f31
Denix7 <dennis.brian.pv@gmail.com>
2020-04-04 23:21:38 -0400

    Estructura inicial del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './components/about/about.component';
+import { InformacionComponent } from './components/informacion/informacion.component';
+import { CuidadosComponent } from './components/cuidados/cuidados.component';
+import { ProyeccionComponent } from './components/proyeccion/proyeccion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    InformacionComponent,
+    CuidadosComponent,
+    ProyeccionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/proyeccion/proyeccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proyeccion',
+  templateUrl: './proyeccion.component.html',
+  styleUrls: ['./proyeccion.component.css']
+})
+export class ProyeccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
