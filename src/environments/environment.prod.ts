28ea71c140b56585e2a330748d910337d690c79b
Denix7 <dennis.brian.pv@gmail.com>
2020-07-01 20:09:48 -0400

    Add environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
   digalogflow: {
     angularBot: 'e8112f2a78e84d9492c2f8ae0a904570'
   },
-  configFirebase: {
-    apiKey: "AIzaSyBdSRQ_agIr23fd-cgDrFk8mKuega9aojk",
-    authDomain: "covid19cuestionario.firebaseapp.com",
-    databaseURL: "https://covid19cuestionario.firebaseio.com",
-    projectId: "covid19cuestionario",
-    storageBucket: "covid19cuestionario.appspot.com",
-    messagingSenderId: "16745715146",
-    appId: "1:16745715146:web:b2310d8fc4019d46802b33",
-    measurementId: "G-9SYSVN13RH"
+  configFirebase : {
+    apiKey: "AIzaSyD-8Hn4O9ZAtVXvHTKC6xt_IAU-lAnq_uI",
+    authDomain: "ngcovidbol.firebaseapp.com",
+    databaseURL: "https://ngcovidbol.firebaseio.com",
+    projectId: "ngcovidbol",
+    storageBucket: "ngcovidbol.appspot.com",
+    messagingSenderId: "748034055390",
+    appId: "1:748034055390:web:9af71abe43e0af4eb6764f",
+    measurementId: "G-SC5YNXZQSC"
   }
 };
